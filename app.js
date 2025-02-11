c0515a7299408abe45563c23a05ca69d9e66040b
Marlon <marlonsilvavvv@gmail.com>
2025-02-10 23:31:56 -0300

    teste7

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
+const path = require('path');
 const wppconnect = require('@wppconnect-team/wppconnect');
-
 
 wppconnect
   .create({
@@ -13,43 +13,45 @@ wppconnect
     },
     catchQR: (base64Qr, asciiQR) => {
       console.log(asciiQR); // Optional to log the QR in the terminal
-      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-        response = {};
 
+      // Regular expression to decode base64 image data
+      const matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
       if (matches.length !== 3) {
         return new Error('Invalid input string');
       }
-      response.type = matches[1];
-      response.data = new Buffer.from(matches[2], 'base64');
 
-      var imageBuffer = response;
-      require('fs').writeFile(
-        'out.png',
-        imageBuffer['data'],
-        'binary',
-        function (err) {
-          if (err != null) {
-            console.log(err);
-          }
+      const response = {
+        type: matches[1],
+        data: Buffer.from(matches[2], 'base64')
+      };
+
+      const imageBuffer = response.data;
+      
+      // Saving the QR code image as out.png in the current directory
+      fs.writeFile(path.join(__dirname, 'out.png'), imageBuffer, 'binary', (err) => {
+        if (err) {
+          console.log('Error saving the QR code image:', err);
+        } else {
+          console.log('QR code image saved as out.png');
         }
-      );
+      });
     },
     logQR: false,
   })
   .then((client) => start(client))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log('Error initializing WPPConnect:', error));
 
-
-  function start(client) {
-    client.onMessage((message) => {
-      if (message.body === 'Oi') {
-        client
-          .sendText(message.from, 'Oi')
-          .then((result) => {
-            console.log('Result: ', result); //return object success
-          })
-          .catch((erro) => {
-            console.error('Error when sending: ', erro); //return object error
-          });
-      }
-    });}
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Oi') {
+      client
+        .sendText(message.from, 'Oi')
+        .then((result) => {
+          console.log('Message sent successfully:', result);
+        })
+        .catch((error) => {
+          console.error('Error when sending message:', error);
+        });
+    }
+  });
+}
