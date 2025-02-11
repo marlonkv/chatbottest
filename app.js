9dc4e32c5320595626372c419aec375f6b620d65
Marlon <marlonsilvavvv@gmail.com>
2025-02-10 21:32:36 -0300

    test6

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const fs = require('fs');
 const wppconnect = require('@wppconnect-team/wppconnect');
 
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium', // Caminho do Chromium no Render
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-
-  const page = await browser.newPage();
-  await page.goto('https://chatbottest-fc87.onrender.com');
-
-  console.log(await page.title());
-  await browser.close();
-})();
-
-
 
 wppconnect
   .create({
     session: 'sessionName',
+    headless: true,
+    useChrome: false,
+    puppeteerOptions: {
+      executablePath: '/usr/bin/chromium',
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    },
     catchQR: (base64Qr, asciiQR) => {
       console.log(asciiQR); // Optional to log the QR in the terminal
       var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
