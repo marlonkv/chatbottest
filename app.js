c340121d86e9448b39892a77d9b1968419aba87a
Marlon <marlonsilvavvv@gmail.com>
2025-02-11 22:46:50 -0300

    first commit

--- app.js
+++ app.js
@@ -1,48 +1,43 @@
+const fs = require('fs');
 const wppconnect = require('@wppconnect-team/wppconnect');
-
 wppconnect
   .create({
     session: 'sessionName',
-    headless: true,
-    useChrome: false,
-    puppeteerOptions: {
-      executablePath: '/usr/bin/chromium-browser', // Caminho para o Chromium já instalado
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    },
     catchQR: (base64Qr, asciiQR) => {
-      console.log(asciiQR); // Logar QR Code no terminal
-      const matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
       if (matches.length !== 3) {
         return new Error('Invalid input string');
       }
-      const response = {
-        type: matches[1],
-        data: Buffer.from(matches[2], 'base64')
-      };
-
-      const imageBuffer = response.data;
-      require('fs').writeFile('out.png', imageBuffer, 'binary', (err) => {
-        if (err) {
-          console.log(err);
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+      var imageBuffer = response;
+      require('fs').writeFile(
+        'out.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
         }
-      });
+      );
     },
     logQR: false,
   })
   .then((client) => start(client))
-  .catch((error) => console.log('Erro ao iniciar o WPPConnect:', error));
-
-function start(client) {
-  client.onMessage((message) => {
-    if (message.body === 'Oi') {
-      client
-        .sendText(message.from, 'Oi')
-        .then((result) => {
-          console.log('Mensagem enviada:', result);
-        })
-        .catch((error) => {
-          console.error('Erro ao enviar mensagem:', error);
-        });
-    }
-  });
-}
+  .catch((error) => console.log(error));
+  function start(client) {
+    client.onMessage((message) => {
+      if (message.body === 'Oi') {
+        client
+          .sendText(message.from, 'Oi')
+          .then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+      }
+    });}
